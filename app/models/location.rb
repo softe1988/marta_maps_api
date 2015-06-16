9cd472d41f3ee66b117610fdd75865a150761f55
Simone Battiste-Alleyne <softe1988@gmail.com>
2015-06-16 18:39:48 +0000

    added search ability

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,9 @@
 class Location < ActiveRecord::Base
+geocoded_by :my_location
+   # can also be an IP address
+before_validation :geocode          # auto-fetch coordinates
+
+def my_location
+    "#{address}, #{city}, GA"
+end
 end
